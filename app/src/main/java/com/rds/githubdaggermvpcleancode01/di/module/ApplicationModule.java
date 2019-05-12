a3168ce2034b4dd6d0036c288d3d31316aa745f7
Andjaradji <andjaradji.rooseno@gmail.com>
2019-05-12 12:53:07 +0700

    Clean some unused import

--- app/src/main/java/com/rds/githubdaggermvpcleancode01/di/module/ApplicationModule.java
+++ app/src/main/java/com/rds/githubdaggermvpcleancode01/di/module/ApplicationModule.java
@@ -3,11 +3,9 @@ package com.rds.githubdaggermvpcleancode01.di.module;
 import android.app.Application;
 import android.content.Context;
 
-import com.rds.githubdaggermvpcleancode01.data.DataManager;
 import com.rds.githubdaggermvpcleancode01.data.network.NetworkService;
 import com.rds.githubdaggermvpcleancode01.di.ApplicationContext;
-import com.rds.githubdaggermvpcleancode01.ui.home.HomePresenter;
-import com.rds.githubdaggermvpcleancode01.ui.home.HomePresenterContract;
+
 
 import javax.inject.Singleton;
 
